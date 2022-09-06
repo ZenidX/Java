13750bb32228125ca6d42572f85d91cab14ed7f1
ZenidX <zenid77@gmail.com>
2022-09-06 13:19:29 +0200

    todo

--- Ejercicio45/src/com/main/Main.java
+++ Ejercicio45/src/com/main/Main.java
@@ -4,18 +4,21 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
+	static Connection connection;
 	public static void main(String[] args) {
-		Connection connection;
+		
 		final String url = "jdbc:mysql://localhost:3306/db_test";
 		final String user = "root";
 		final String pass = "DoomEternal";
+		connection = createConnection(url, user, pass);
 		try {
-			connection = DriverManager.getConnection(url, user, pass);
+
 			Statement statement = connection.createStatement();
-			ResultSet result = statement.executeQuery("SELECT * FROM db_test.usuarios"); 
+			ResultSet result = statement.executeQuery("SELECT * FROM db_test.usuarios");
 			ResultSetMetaData rsmd = result.getMetaData();
 			int columnCount = rsmd.getColumnCount();
 			for (int i = 1; i < columnCount; i++) {
@@ -30,11 +33,39 @@ public class Main {
 				int edad = result.getInt("Edad");
 				System.out.printf("%10d%10s%10d%n", id_usuarios, nombre, edad);
 			}
-			result.close(); // Cerrando resultset
-			statement.close(); // Cerrando statement
-			connection.close(); // Cerrando connection
+			result.close();
+			statement.close();
+			connection.close();
 		} catch (Exception e) {
 			System.err.println(e);
 		}
+	}
+
+	public static Connection createConnection(String url, String user, String pass) {
+		Connection cn = null;
+		try {
+			cn = DriverManager.getConnection(url, user, pass);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return cn;
+	}
+
+	public void Create(Connection connection) {
+//		Statement statement = connection.createStatement();
+//		ResultSet result = statement.executeQuery("SELECT * FROM db_test.usuarios");
+
+	}
+
+	public void Read() {
+
+	}
+
+	public void Update() {
+
+	}
+
+	public void Delete() {
+
 	}
 }
